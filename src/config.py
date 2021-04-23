6dc6bc0a83c98c8b28bc3fe621d5590e5b0a73dc
331leo <331leo@outlook.com>
2021-04-24 01:14:38 +0900

    feat: Style Changed, google_gtag from env

--- src/config.py
+++ src/config.py
@@ -5,10 +5,11 @@ mongo_host = "localhost"
 mongo_port = 27017
 authcode = "authcode"
 logconfig = "logging_local.json"
-adsense_client = "ca-pub-1463419267666050"
+google_gtag = "G-GOOGLE_GTAG"
 #도커환경
 if os.environ.get('DOCKER-MONGO-HOST'):
     mongo_host = str(os.environ.get('DOCKER-MONGO-HOST'))
     mongo_port = int(os.environ.get('DOCKER-MONGO-PORT'))
     authcode = str(os.environ.get('DOCKER-AUTHCODE'))
+    google_gtag = str(os.environ.get("DOCKER-GOOGLE-GTAG"))
     logconfig = "logging_docker.json"
